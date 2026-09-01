3e2a1c2b63ee20541e6d4a93cc7878a046a49de2
arthur-staseljun <arthur.staseljun@gmail.com>
2026-09-01 18:05:05 +0300

    initialised spring project

---
+++ src/main/java/org/sun/racing/RacingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sun.racing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sun/racing/RacingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sun.racing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
